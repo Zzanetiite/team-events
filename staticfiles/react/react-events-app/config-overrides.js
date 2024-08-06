5a4634fbb08a897d56103a9adcaff60d91a651a1
Zanete Dijeva <zanete.dijeva@gmail.com>
2024-08-06 22:48:10 +0100

    Ignore static files

--- staticfiles/react/react-events-app/config-overrides.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-
-module.exports = function override(config, env) {
-  config.output.path = path.resolve(__dirname, '../');
-  return config;
-};
--- staticfiles/react/react-events-app/src/interfaces/types.d.ts
+++
@@ -1,8 +0,0 @@
-export interface Counter {
-  id: number;
-  value: number;
-}
-
-export interface CounterButtonProps {
-  counterId: number;
-}
