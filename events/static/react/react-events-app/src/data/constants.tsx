447d6f822a1ea3a47d4b1618d3cfafee3d0aa531
Zanete Dijeva <zanete.dijeva@gmail.com>
2024-08-09 23:48:03 +0100

    Move React app to a separate repository

--- events/static/react/react-events-app/src/data/constants.tsx
+++
@@ -1,2 +0,0 @@
-export const DOMAIN = window.DOMAIN || 'http://localhost:8000';
-export const REACT_BASENAME = window.REACT_BASENAME || '/static/react';
--- events/static/react/react-events-app/src/global.d.ts
+++
@@ -1,9 +0,0 @@
-// src/global.d.ts
-export {};
-
-declare global {
-  interface Window {
-    REACT_BASENAME?: string;
-    DOMAIN?: string;
-  }
-}
