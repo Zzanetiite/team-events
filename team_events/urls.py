d3ae2f66c768914394903333f8cf77742285762b
Zanete Dijeva <zanete.dijeva@gmail.com>
2024-08-12 18:51:56 +0100

    Re-route React urls to Django

--- team_events/urls.py
+++ team_events/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("events.urls")),
+    # Re-route all routes for the React app
+    re_path(r"^(?:.*)/?$", TemplateView.as_view(template_name="index.html")),
 ]
