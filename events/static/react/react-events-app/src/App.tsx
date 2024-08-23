78b64f44ea06db422232fb792d61805d1530b7d8
Zanete Dijeva <zanete.dijeva@gmail.com>
2024-08-23 14:18:44 +0100

    Move router to its own file

--- events/static/react/react-events-app/src/App.tsx
+++ events/static/react/react-events-app/src/App.tsx
@@ -1,84 +1,9 @@
 import React from 'react';
 import './App.css';
 import { TokenProvider } from './context/TokenContext';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import NotFound from './components/common/NotFound';
+import { RouterProvider } from 'react-router-dom';
 import PrimarySearchAppBar from './components/layout/NavBar';
-import UserForm from './components/profile/UserForm';
-import { ApiEndpoints } from './constants';
-import ValidateAdminPasswordForm from './components/profile/ValidateAdminPassword';
-import Home from './pages/Home';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/createadmin',
-    element: (
-      <UserForm
-        title="Create Admin User"
-        apiEndpoint={ApiEndpoints.CREATE_ADMIN_USER}
-        adminPage={true}
-        successMessageText={
-          <span>
-            Success! Admin user created successfully.{' '}
-            <a href="/login">Go to Login</a>
-          </span>
-        }
-        messageForBadRequest="Admin user already exists. Please choose a different username."
-      />
-    ),
-  },
-  {
-    path: '/validateadmin',
-    element: (
-      <ValidateAdminPasswordForm
-        title="Please enter secret password"
-        apiEndpoint={ApiEndpoints.VALIDATE_ADMIN_PAGE_PASSWORD}
-        successMessageText={<span>Success! Password correct.</span>}
-      />
-    ),
-  },
-  {
-    path: '/signup',
-    element: (
-      <UserForm
-        title="Sign Up"
-        apiEndpoint={ApiEndpoints.CREATE_USER}
-        successMessageText={
-          <span>
-            Success! User created successfully. <a href="/login">Go to Login</a>
-          </span>
-        }
-        messageForBadRequest="User already exists. Please choose a different username."
-      />
-    ),
-  },
-  {
-    path: '/login',
-    element: (
-      <UserForm
-        title="Login"
-        apiEndpoint={ApiEndpoints.LOGIN}
-        method="POST"
-        successMessageText={<span>Login success! Redirecting ...</span>}
-        messageForBadRequest="Invalid credentials. Please try again."
-        loginPage={true}
-      />
-    ),
-  },
-  {
-    // Non-existent routes display Not Found page
-    path: '/*',
-    element: (
-      <div>
-        <NotFound />
-      </div>
-    ),
-  },
-]);
+import router from './router';
 
 const App: React.FC = () => {
   return (
