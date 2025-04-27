a938fe76a264889755f4604f1c3cdef1011fb9db
Zanete Dijeva <zanete.dijeva@gmail.com>
2025-04-27 15:34:06 +0100

    Fix Rating visuals in the small card

--- react-events-app/src/components/common/rating/AverageRating.tsx
+++ react-events-app/src/components/common/rating/AverageRating.tsx
@@ -1,21 +1,38 @@
 import * as React from 'react';
 import { RatingProps } from '../../../interfaces/types';
 import { Star, VolumeUp } from '@mui/icons-material';
-import { Tooltip } from '@mui/material';
+import { Box, Tooltip } from '@mui/material';
 import Row from '../../base/Row';
+import { COLOR_SCHEME } from '../../../constants/Color';
 
 const AverageRatings: React.FC<RatingProps> = ({ event }) => {
+  const ratingItemStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    padding: '1px 8px',
+    borderRadius: '8px',
+    backgroundColor: COLOR_SCHEME.LightGrey,
+    userSelect: 'none',
+    cursor: 'default',
+  };
+
   return (
     <Row>
-      <Tooltip key="volume" title="Average volume rating for this place=">
-        <div>
-          <VolumeUp fontSize="inherit" /> {event.loudnessRating}
-        </div>
+      <Tooltip key="volume" title="Average loudness rating for this place">
+        <Box sx={ratingItemStyle} color={COLOR_SCHEME.MediumBlue}>
+          <VolumeUp fontSize="small" color="inherit" />
+          <Box ml={0.5} color="text.primary">
+            {event.loudnessRating.toFixed(1)}
+          </Box>
+        </Box>
       </Tooltip>
-      <Tooltip key="place" title="Average place rating for this place">
-        <div>
-          <Star fontSize="inherit" /> {event.placeRating}
-        </div>
+      <Tooltip key="place" title="Average experience rating for this place">
+        <Box sx={ratingItemStyle} color={COLOR_SCHEME.BrightYellow}>
+          <Star fontSize="small" color="inherit" />
+          <Box ml={0.5} color="text.primary">
+            {event.placeRating.toFixed(1)}
+          </Box>
+        </Box>
       </Tooltip>
     </Row>
   );
---
+++ react-events-app/src/constants/Color.ts
@@ -0,0 +1,6 @@
+export enum COLOR_SCHEME {
+  LightGrey = '#f5f5f5',
+  BrightYellow = '#ffd600',
+  LightBlue = '#cdf0f4',
+  MediumBlue = '#33a1b0',
+}
