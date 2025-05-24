e1fd9807920e0b8d56d06c30f06a27d536cc0f8c
Zanete Dijeva <zanete.dijeva@gmail.com>
2025-05-24 16:38:23 +0100

    Debug Render

--- start.sh
+++ start.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e  # Exit on any error
 
+echo "PORT=$PORT"
+echo "DEBUG=$DEBUG"
+echo "ALLOWED_HOSTS=$ALLOWED_HOSTS"
+echo "STATIC_ROOT=$STATIC_ROOT"
+echo "SECURE_SSL_REDIRECT=$SECURE_SSL_REDIRECT"
+echo "SECURE_PROXY_SSL_HEADER=$SECURE_PROXY_SSL_HEADER"
+ls -la $STATIC_ROOT
+
 echo "Running collectstatic..."
 python manage.py collectstatic --noinput
 
