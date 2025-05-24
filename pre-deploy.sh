ddaf1dcf69609972495a8805ed7af11a9fb33bed
Zanete Dijeva <zanete.dijeva@gmail.com>
2025-05-24 14:35:19 +0100

    Update pre-deploy

--- pre-deploy.sh
+++ pre-deploy.sh
@@ -3,11 +3,9 @@
 # Exit script on any error
 set -e
 
-echo "DATABASE_URL is: $DATABASE_URL"
-
 echo "===================================="
 echo "Collecting static files for Django..."
-RUN python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 echo "===================================="
 echo "Success! Pre-Deploy script complete."
